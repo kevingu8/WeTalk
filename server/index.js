2bb4dfe0f7ec16cf86bc6c52e2e12ac8a2285e86
Kevin Gu <132306047+kevingu8@users.noreply.github.com>
2023-05-08 11:33:26 -0400

    Add files via upload
    First commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true}));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: {"private-key": "cfd3e181-f555-431c-b5f9-118f30acd1f4" }}
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
+});
+
+app.listen(3001);
